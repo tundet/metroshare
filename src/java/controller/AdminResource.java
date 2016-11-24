db3b50e7d31ccd4c2c3c81c8504241365b4634af
Mafields <Mafields86@gmail.com>
2016-11-24 12:57:33 +0200

    New style JSON building admin v0.5

--- src/java/controller/AdminResource.java
+++ src/java/controller/AdminResource.java
@@ -5,8 +5,17 @@
  */
 package controller;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -65,24 +74,28 @@ public class AdminResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getUsersJson() {
         //TODO return proper representation object
-        String ul = "{ \"users\": [";
 
         List<User> ulst = mssb.readAllUsers();
-        // user found and returning json data about login priviliges and activity
+        // user found and returning json data about login priviliges and activity        
+        JsonArrayBuilder builder = Json.createArrayBuilder();
         for (User u : ulst) {
-            ul += "{\"id\": \"" + u.getId() + "\", ";
-            ul += "\"username\": \"" + u.getLogin() + "\", ";
-            ul += "\"password\": \"" + u.getPassword() + "\", ";
-            ul += "\"privileges\": \"" + u.getPrivileges() + "\", ";
-            ul += "\"activity\": \"" + u.getActivity() + "\", ";
-            if (u == ulst.get(ulst.size() - 1)) {
-                ul += "\"sessionid\": \"" + u.getSessionID() + "\"} ";
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("ID", u.getId());
+            job.add("Login", u.getLogin());
+            job.add("password", u.getPassword());
+            job.add("privileges", u.getPrivileges());
+            job.add("activity", u.getActivity().toString());
+            if (u.getSessionID() != null) {
+                job.add("sessionID", u.getSessionID());
             } else {
-                ul += "\"sessionid\": \"" + u.getSessionID() + "\"}, ";
+                job.add("sessionID", 0);
             }
+            JsonObject userValue = job.build();
+            builder.add(userValue);
         }
-        ul += "]}";
-        return ul;
+        JsonArray userA = builder.build();
+
+        return userA.toString();
     }
 
     /**
@@ -95,23 +108,22 @@ public class AdminResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getCommentsJson() {
         //TODO return proper representation object
-        String cl = "{ \"comments\": [";
-
         List<Comment> clst = mssb.readAllComments();
+
         // user found and returning json data about login priviliges and activity
+        JsonArrayBuilder builder = Json.createArrayBuilder();
         for (Comment c : clst) {
-            cl += "{\"id\": \"" + c.getId() + "\", ";
-            cl += "\"mediaid\": \"" + c.getMediaId() + "\", ";
-            cl += "\"userid\": \"" + c.getUserId() + "\", ";
-            cl += "\"message\": \"" + c.getMessage() + "\", ";
-            if (c == clst.get(clst.size() - 1)) {
-                cl += "\"date\": \"" + c.getDate() + "\"} ";
-            } else {
-                cl += "\"date\": \"" + c.getDate() + "\"}, ";
-            }
+            JsonObject commentValue = Json.createObjectBuilder()
+                    .add("ID", c.getId())
+                    .add("login", c.getUserId().getLogin())
+                    .add("mediaId", c.getMediaId().getId())
+                    .add("message", c.getMessage())
+                    .add("date", c.getDate().toString()).build();
+            builder.add(commentValue);
         }
-        cl += "]}";
-        return cl;
+        JsonArray commentA = builder.build();
+
+        return commentA.toString();
     }
 
     /**
@@ -124,42 +136,70 @@ public class AdminResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getMediasJson() {
         //TODO return proper representation object
-        String ml = "{ \"medias\": [";
-
         List<Media> mlst = mssb.readAllMedias();
+
         // user found and returning json data about login priviliges and activity
+        JsonArrayBuilder builder = Json.createArrayBuilder();
         for (Media m : mlst) {
-            ml += "{\"id\": \"" + m.getId() + "\", ";
-            ml += "\"userid\": \"" + m.getUserId() + "\", ";
-            ml += "\"medialocation\": \"" + m.getMediaLocation() + "\", ";
-            ml += "\"date\": \"" + m.getDate() + "\", ";
-            ml += "\"nsfw\": \"" + m.getNsfw() + "\", ";
-            if (m == mlst.get(mlst.size() - 1)) {
-                ml += "\"tags\": [";
-                for (MediaTag mt : m.getMediaTagCollection()) {
-                    ml += "{\"tagid\": \"" + mt.getTagId().getId() + "\", ";
-                    if (mt == m.getMediaTagCollection().toArray()[m.getMediaTagCollection().size()-1]) {
-                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" }";
-                    } else {
-                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" },";
-                    }
-                }
-                ml += "]}";
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("ID", m.getId());
+            job.add("Login", m.getUserId().getLogin());
+            job.add("medialocation", m.getMediaLocation());
+            job.add("nsfw", m.getNsfw());
+            job.add("date", m.getDate().toString());
+            if (m.getTitle() != null) {
+                job.add("sessionID", m.getTitle());
             } else {
-                ml += "\"tags\": [";
-                for (MediaTag mt : m.getMediaTagCollection()) {
-                    ml += "{\"tagid\": \"" + mt.getTagId().getId() + "\", ";
-                    if (mt == m.getMediaTagCollection().toArray()[m.getMediaTagCollection().size()-1]) {
-                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" }";
-                    } else {
-                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" },";
-                    }
-                }
-                ml += "]},";
+                job.add("sessionID", m.getDate().toString());
             }
+            JsonArrayBuilder tagAbuilder = Json.createArrayBuilder();
+            for (MediaTag mt : m.getMediaTagCollection()) {
+                JsonObjectBuilder tagjob = Json.createObjectBuilder();
+                tagjob.add("tagid", mt.getTagId().getId());
+                tagjob.add("tag", mt.getTagId().getTag());
+                tagAbuilder.add(tagjob);
+            }
+            job.add("tags", tagAbuilder);
+            JsonObject mediaValue = job.build();
+            builder.add(mediaValue);
         }
-        ml += "]}";
-        return ml;
+        JsonArray mediaA = builder.build();
+
+        return mediaA.toString();
+        
+        // NEVER AGAIN!!!
+//        String ml = "{ \"medias\": [";
+//        for (Media m : mlst) {
+//            ml += "{\"id\": \"" + m.getId() + "\", ";
+//            ml += "\"userid\": \"" + m.getUserId() + "\", ";
+//            ml += "\"medialocation\": \"" + m.getMediaLocation() + "\", ";
+//            ml += "\"date\": \"" + m.getDate() + "\", ";
+//            ml += "\"nsfw\": \"" + m.getNsfw() + "\", ";
+//            if (m == mlst.get(mlst.size() - 1)) {
+//                ml += "\"tags\": [";
+//                for (MediaTag mt : m.getMediaTagCollection()) {
+//                    ml += "{\"tagid\": \"" + mt.getTagId().getId() + "\", ";
+//                    if (mt == m.getMediaTagCollection().toArray()[m.getMediaTagCollection().size() - 1]) {
+//                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" }";
+//                    } else {
+//                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" },";
+//                    }
+//                }
+//                ml += "]}";
+//            } else {
+//                ml += "\"tags\": [";
+//                for (MediaTag mt : m.getMediaTagCollection()) {
+//                    ml += "{\"tagid\": \"" + mt.getTagId().getId() + "\", ";
+//                    if (mt == m.getMediaTagCollection().toArray()[m.getMediaTagCollection().size() - 1]) {
+//                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" }";
+//                    } else {
+//                        ml += "\"tag\": \"" + mt.getTagId().getTag() + "\" },";
+//                    }
+//                }
+//                ml += "]},";
+//            }
+//        }
+//        ml += "]}";
     }
 
     /**
@@ -172,20 +212,19 @@ public class AdminResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getTagsJson() {
         //TODO return proper representation object
-        String tl = "{ \"tags\": [";
-
         List<Tag> tlst = mssb.readAllTags();
+        
         // user found and returning json data about login priviliges and activity
+        JsonArrayBuilder builder = Json.createArrayBuilder();
         for (Tag t : tlst) {
-            tl += "{\"id\": \"" + t.getId() + "\", ";
-            if (t == tlst.get(tlst.size() - 1)) {
-                tl += "\"tag\": \"" + t.getTag() + "\"} ";
-            } else {
-                tl += "\"tag\": \"" + t.getTag() + "\"}, ";
-            }
+            JsonObject tagValue = Json.createObjectBuilder()
+                    .add("ID", t.getId())
+                    .add("tag", t.getTag()).build();
+            builder.add(tagValue);
         }
-        tl += "]}";
-        return tl;
+        JsonArray tagA = builder.build();
+        
+        return tagA.toString();
     }
     
     @POST
