75aec0bf5b2bfee6943ac81f385ecdfda0ae140d
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2016-12-08 09:26:27 +0200

    Remove autogenerated boilerplate Javadoc

--- src/java/model/MediaLike.java
+++ src/java/model/MediaLike.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.Serializable;
@@ -23,10 +18,6 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author Mafields
- */
 @Entity
 @Table(name = "media_like")
 @XmlRootElement
